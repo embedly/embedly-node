7f7d9ea95c60a3be074099b1d118fc22fa191f8b
Bob Corsaro <bob@embed.ly>
2011-02-07 21:48:53 -0500

    Adds multi-url test

--- test/oembed-test.js
+++ test/oembed-test.js
@@ -72,7 +72,7 @@ var oembed_provider_url_vows = {}
         }
       )
     }
-    , 'reponds with provider_url': assertObjValue('provider_url', expected)
+    , 'reponds with expected provider_url': assertObjValue('provider_url', expected)
   }
 })
 
@@ -103,9 +103,67 @@ var oembed_type_vows = {}
         }
       )
     }
-    , 'reponds with type': assertObjValue('type', expected)
+    , 'reponds with expected type': assertObjValue('type', expected)
   }
 })
+
+/*
+ *  Scenario Outline: Get the provider_url with force flag
+ *      Given an embedly endpoint
+ *      When oembed is called with the <url> URL and force flag
+ *      Then the provider_url should be <provider_url>
+ */
+var oembed_provider_with_force_vows = {}
+/*
+ * Examples:
+ ** url                                        | provider_url            */
+;[" http://www.youtube.com/watch?v=Zk7dDekYej0 | http://www.youtube.com/ "
+].forEach(function(line) {
+  var parts = line.split('|')
+    , url = parts[0].trim()
+    , expected = parts[1].trim()
+
+  oembed_provider_with_force_vows['when oembed is called with force for provider with url '+url] = {
+    topic: function (api) {
+      return api.oembed(
+        { params:{'url': url, 'force': 'true'}
+        , complete: this.callback
+        }
+      )
+    }
+    , 'reponds with expected provider_url': assertObjValue('provider_url', expected)
+  }
+})
+
+/*
+ *  Scenario Outline: Get multiple provider_urls
+ *      Given an embedly endpoint
+ *      When oembed is called with the <urls> URLs
+ *      Then provider_url should be <provider_urls>
+ */
+var oembed_multiple_provider_vows = {}
+/*
+ * Examples:
+ ** urls                                                                                      | provider_urls                                 */
+;[" http://www.scribd.com/doc/13994900/Easter,http://www.scribd.com/doc/28452730/Easter-Cards | http://www.scribd.com/,http://www.scribd.com/ "
+, " http://www.youtube.com/watch?v=Zk7dDekYej0,http://plixi.com/p/16044847                    | http://www.youtube.com/,http://plixi.com      "
+].forEach(function(line) {
+  var parts = line.split('|')
+    , urls = parts[0].trim()
+    , expected = parts[1].trim()
+
+  oembed_multiple_provider_vows['when oembed is called for provider with multi-urls '+urls] = {
+    topic: function (api) {
+      return api.oembed(
+        { params:{'urls': urls.split(','), 'force': 'true'}
+        , complete: this.callback
+        }
+      )
+    }
+    , 'reponds with expected provider_url': assertObjValue('provider_url', expected.split(','))
+  }
+})
+
 
 /*
  *  Scenario Outline: Get the provider_url with pro
@@ -133,7 +191,7 @@ var oembed_pro_provider_vows = {}
         }
       )
     }
-    , 'reponds with provider_url': assertObjValue('provider_url', expected)
+    , 'reponds with expected provider_url': assertObjValue('provider_url', expected)
   }
 })
 
@@ -146,6 +204,8 @@ vows.describe('OEmbed').addBatch({
     }).
     merge(oembed_provider_url_vows).
     merge(oembed_type_vows).
+    merge(oembed_provider_with_force_vows).
+    merge(oembed_multiple_provider_vows).
     end
   , 'A Pro API Instance': Hash({
       topic: new(embedly.api)({key: process.env.EMBEDLY_KEY})

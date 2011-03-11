1e6759381589bb6cd58afb1c0672122faec4483f
Bob Corsaro <bob@embed.ly>
2011-03-11 13:55:59 -0500

    services working

---
+++ test/services-test.js
@@ -0,0 +1,45 @@
+/*
+ * Feature: Objectify
+ *
+ *   As an embedly user
+ *   I want to call the the embedly api
+ *   Because I want to objectify a url
+ */
+
+var path = require('path')
+
+require.paths.unshift(path.join(__dirname, '../lib'))
+
+var embedly = require('embedly')
+  , vows = require('vows')
+  , assert = require('assert')
+  , Hash = embedly.utils.require_either('hashish', 'traverse/hash')
+  , util = embedly.utils.require_either('util', 'utils')
+
+
+/*
+ * HELPERS
+ */
+function canonize_value(val) {
+  return (typeof(val) == 'undefined' || val == null) ?
+           '' 
+         : val
+}
+
+/*
+ * Build vows
+ */
+vows.describe('Services').addBatch(
+{ 'A Pro API Instance':
+  { topic: new(embedly.Api)()
+  , 'gets services':
+    { topic: function(api) {
+        return api.services().on('complete', this.callback).start()
+      }
+    , 'response with services': function(repl) {
+        assert.isTrue(typeof(repl) == 'object')
+      }
+    }
+  }
+}).export(module, {error: false})
+
